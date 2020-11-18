d0b4cd3258791aa0be6a61ebf71b0943c6f4db93
Naveen Ramasamy <naveenramasamy@Naveens-MacBook-Pro.local>
2020-11-18 14:28:47 +0530

    added jenkinsfile

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 wget https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip
 unzip terraform_0.13.4_linux_amd64.zip
 chmod +x terraform
-sudo cp terraform /usr/bin/terraform
+ls -l
+cp terraform /usr/local/bin/terraform
 terraform version
 echo $PATH
 
