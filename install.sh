1a8df98cebf482a75648533fda15910fc6dcfef0
Naveen Ramasamy <naveenramasamy@Naveens-MacBook-Pro.local>
2020-11-18 14:24:34 +0530

    added jenkinsfile

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install terraform
+wget https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip
+unzip terraform_0.13.4_linux_amd64.zip
+chmod +x terraform
+sudo cp terraform /usr/bin/terraform
+terraform version
+echo $PATH
+
+# Install jq
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+mv jq-linux64 jq
+chmod +x jq
